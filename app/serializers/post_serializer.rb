4f47b6f2d943869b6fb9cb99b02ec1f4a354cc95
Michael Cheng <cheng.c.mike@gmail.com>
2018-11-02 13:50:04 -0500

    add related_tags

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,13 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :published, :slug, :url, :title, :description, :subtitle, :image_url, :image_url_small, :references, :html, :related_posts_tags, :date_added, :tags
+  attributes :id, :published, :slug, :url, :title, :description, :subtitle, :image_url, :image_url_small, :references, :html, :related_posts_tags, :mapped_related_posts_tags, :date_added, :tags
+
+  def mapped_related_posts_tags
+    if self.object.related_posts_tags
+      return self.object.related_posts_tags.split(',').map do |tagId|
+        Tag.find(tagId)
+      end
+    end
+
+    return []
+  end
 end
