32eee552d0f9101d875ad9f8edf7f32efa3463dd
Michael Cheng <cheng.c.mike@gmail.com>
2018-11-08 16:02:26 -0600

    add user

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :published, :slug, :url, :title, :description, :subtitle, :image_url, :image_url_small, :references, :html, :related_posts_tags, :mapped_related_posts_tags, :date_added, :updated_at, :tags, :sorted_tags
+  attributes :id, :published, :slug, :url, :title, :description, :subtitle, :image_url, :image_url_small, :references, :html, :related_posts_tags, :mapped_related_posts_tags, :date_added, :updated_at, :tags, :sorted_tags, :user
 
   def mapped_related_posts_tags
     if self.object.related_posts_tags
