3c8bd301fb3f53c65a4d74b20d344075ba75e374
Michael Cheng <cheng.c.mike@gmail.com>
2018-11-12 17:47:21 -0600

    add post_ids to tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -9,7 +9,8 @@ class Tag < ApplicationRecord
       url: url,
       title: title,
       description: description,
-      subtitle: subtitle
+      subtitle: subtitle,
+      post_ids: posts.map { |post| post.id }
     }
   end
 end
