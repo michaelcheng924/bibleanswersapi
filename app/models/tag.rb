f472b9ba3326c477c45ba73c463a652505da503a
Michael Cheng <cheng.c.mike@gmail.com>
2018-11-19 12:57:01 -0600

    remove serializers

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,19 @@
 class Tag < ApplicationRecord
   has_many :post_tags
   has_many :posts, through: :post_tags
+
+  def as_json(options = {})
+    {
+      id: id,
+      slug: slug,
+      url: url,
+      title: title,
+      description: description,
+      subtitle: subtitle,
+      image_url: image_url,
+      date_added: date_added,
+      updated_at: updated_at,
+      post_slugs: posts.map(&:slug)
+    }
+  end
 end
--- app/serializers/tag_serializer.rb
+++
@@ -1,7 +0,0 @@
-class TagSerializer < ActiveModel::Serializer
-  attributes :id, :slug, :url, :title, :description, :subtitle, :image_url, :date_added, :updated_at, :post_slugs
-
-  def post_slugs
-    self.object.posts.map(&:slug)
-  end
-end
